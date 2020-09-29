34883916d6f5d455088e7de73ec92271791efec7
why <1273695055@qq.com>
2020-09-29 21:37:33 +0800

    第一次提交-空项目

---
+++ app/src/main/java/com/lkm/lkmydhltest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lkm.lkmydhltest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("呵呵哒");
+    }
+}
